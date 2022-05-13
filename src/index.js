37460e068130a4cc2f9d8a65d52115fd418368d8
simontree <simonroof@tutanota.com>
2022-05-13 17:36:47 +0200

    first commit drummachine branch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import DrumMachine from './components/DrumMachine';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DrumMachine />
   </React.StrictMode>
 );
 
