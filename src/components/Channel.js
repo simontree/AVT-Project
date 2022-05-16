964f831e62c4f8d31a59326f7709d699cdbdcce0
Dohruba <ricardohendrichs@gmail.com>
2022-05-16 19:28:57 +0200

    Working on Channel, Css is weird. Feedback appreciated.

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,13 +1,44 @@
-import logo from './logo.svg';
-import '../App.css';
+import './Channel.css'
+import React from 'react';
 
 function Channel() {
 
   return (
-    <div className="Channel">
-      
+    <div className="channel">
+      <div className='switchContainer'>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+      </div>
+      <div className='fileTitle'>
+        <p>Placeholder for var</p>
+      </div>
+      <div className='channelContext'>
+        <button>...</button>
+      </div>
+      <div className='channelPlay'>
+        <button className='playButton'>Play</button>
+      </div>
+      <div className='volIcon'>
+        <p>Vol</p>
+      </div>
+      <div className='volSlider'>
+      </div>
+      <div className='speedIcon'>
+        <p>Sp</p>
+      </div>
+      <div className='speedSlider'>
+      </div>
+      <div className='midiText'>Midi Channel</div>
+      <div className='midiChannel1'>O</div>
+      <div className='midiChannel2'>O</div>
+      <div className='midiChannel3'>O</div>
+      <div className='midiChannel4'>O</div>
+
     </div>
   );
 }
 
 export default Channel;
+/* */
