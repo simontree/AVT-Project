0b8b909776056a61530cf3d67c496a50f4b35f64
Dohruba <ricardohendrichs@gmail.com>
2022-05-17 19:41:16 +0200

    Radio buttons, sliders and organization are better

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -11,30 +11,53 @@ function Channel() {
           <span className="slider round"></span>
         </label>
       </div>
+
       <div className='fileTitle'>
         <p>Placeholder for var</p>
       </div>
+
       <div className='channelContext'>
         <button>...</button>
       </div>
+
       <div className='channelPlay'>
         <button className='playButton'>Play</button>
       </div>
+
       <div className='volIcon'>
         <p>Vol</p>
       </div>
       <div className='volSlider'>
+        <input type={'range'} min='0' max='99' 
+        className='vSlider' id="volRange"></input>
       </div>
+
       <div className='speedIcon'>
         <p>Sp</p>
       </div>
       <div className='speedSlider'>
+        <input type={'range'} min='0' max='99' 
+          className='vSlider' id="spRange"></input>
       </div>
+
+
       <div className='midiText'>Midi Channel</div>
-      <div className='midiChannel1'>O</div>
-      <div className='midiChannel2'>O</div>
-      <div className='midiChannel3'>O</div>
-      <div className='midiChannel4'>O</div>
+      <label className='container' id='midiChannel1'>1
+        <input type={'radio'} name="radio" ></input>
+        <span class='checkmark'></span>
+      </label>
+      <label className='container' id='midiChannel2'>2
+        <input type={'radio'} name="radio"></input>
+        <span class='checkmark'></span>
+      </label>
+      <label className='container' id='midiChannel3'>3
+        <input type={'radio'} name="radio"></input>
+        <span class='checkmark'></span>
+      </label>
+      <label className='container' id='midiChannel4'>4
+        <input type={'radio'} name="radio"></input>
+        <span class='checkmark'></span>
+      </label>
 
     </div>
   );
