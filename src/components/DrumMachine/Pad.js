c4a7b3ef6846ae8da092876abac7efd3eb423346
simontree <simonroof@tutanota.com>
2022-05-18 12:44:27 +0200

    added recording feature

--- src/components/DrumMachine/Pad.js
+++ src/components/DrumMachine/Pad.js
@@ -1,20 +1,21 @@
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 
-function Pad({clip, volume}){
+function Pad({clip, volume, setRecording}){
 
     const playSound = () => {
         const audioTag = document.getElementById(clip.keyTrigger);
         audioTag.currentTime = 0;
         audioTag.play();
         audioTag.volume = volume;
+        setRecording(prev => prev + clip.keyTrigger + " ")
     }
 
     return(
       <div>
-        <Button onClick={playSound} className="btn bg-slate-500 p-4 m-3">
+        <button onClick={playSound} className="btn bg-slate-500 p-4 m-3">
         <audio className="clip" id={clip.keyTrigger} src={clip.url}/>
         {clip.keyTrigger}
-        </Button>
+        </button>
       </div>
     )
 };
