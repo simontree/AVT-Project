15393ebd7a3018427b226e2fc5f752d3ef176841
simontree <simonroof@tutanota.com>
2022-07-08 12:43:12 +0200

    simplified - deleted pad.js

--- src/components/DrumMachine/PadRow.js
+++ src/components/DrumMachine/PadRow.js
@@ -1,16 +1,30 @@
-import { propTypes } from 'react-bootstrap/esm/Image';
-import Pad from './Pad';
 /**
  * @param {padClip} - e.g. Kick, Hi-Hat or any other sample that needs to be inserted into row
  */
 function PadRow(props){
     const padToFind = props.audioClips.find((clip) => clip.id === props.padClip);
-    // props.setPad(padToFind.id);
     const padArray = []
-    // console.log(padArray);
+
     const loopPad = () => {
       for(let i=0; i<8;i++){
-        padArray.push(<li><Pad key={padToFind.id} clip={padToFind} volume={props.volume} setPad={props.setPad}/></li>)
+        padArray.push(<li>
+          <button 
+          key={padToFind.id} 
+          className="pads p-4 m-3 box-border h-17 w-20"
+          type="button" 
+          role="switch" 
+          aria-checked={props.ariaChecked} 
+          onClick={props.play} 
+          >
+          <audio 
+          className="clip" 
+          id={padToFind.keyTrigger} 
+          src={padToFind.url}
+          />
+          {padToFind.keyTrigger}
+          </button>
+          </li>
+          )
       };
     };
     loopPad();
@@ -19,8 +33,8 @@ function PadRow(props){
         <section>
             <ul class="grid grid-cols-10">
                 <li class="mt-7">{props.padClip}</li>
-        {padArray}
-        </ul>
+            {padArray}
+            </ul>
         </section>
     );
 }
