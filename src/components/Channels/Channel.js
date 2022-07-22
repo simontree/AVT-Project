9369872f36eac90d8a15c7166257d7dadc3a8b14
Robert <59532700+simontree@users.noreply.github.com>
2022-07-22 12:21:16 +0200

    minor style changes & refactoring

--- src/components/Channels/Channel.js
+++ src/components/Channels/Channel.js
@@ -24,6 +24,7 @@ import VolumeUp from '@mui/icons-material/VolumeUp';
 import SpeedIcon from '@mui/icons-material/Speed';
 import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
+import { AntSwitch } from "./Filters/AntSwitch";
 
 const defaultFilterStrength = 0.05;
 const defaultFilterType = "lowpass";
@@ -314,44 +315,6 @@ function Channel(props) {
     return `${minute}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
   }
 
-  const AntSwitch = styled(Switch)(() => ({
-    width: 28,
-    height: 16,
-    padding: 0,
-    display: 'flex',
-    '&:active': {
-      '& .MuiSwitch-thumb': {
-        width: 15,
-      },
-      '& .MuiSwitch-switchBase.Mui-checked': {
-        transform: 'translateX(9px)',
-      },
-    },
-    '& .MuiSwitch-switchBase': {
-      padding: 2,
-      '&.Mui-checked': {
-        transform: 'translateX(12px)',
-        color: '#fff',
-        '& + .MuiSwitch-track': {
-          opacity: 1,
-          backgroundColor: '#177ddc',
-        },
-      },
-    },
-    '& .MuiSwitch-thumb': {
-      boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
-      width: 12,
-      height: 12,
-      borderRadius: 6,
-    },
-    '& .MuiSwitch-track': {
-      borderRadius: 16 / 2,
-      opacity: 1,
-      backgroundColor:'rgba(255,255,255,.35)',
-      boxSizing: 'border-box',
-    },
-  }));
-
   return (
     <Container
     id={channelID}
@@ -473,7 +436,8 @@ function Channel(props) {
       alignItems="center"
       justifyContent="space-evenly"
       direction='row'
-      spacing={1}>
+      spacing={1}
+      sx={{marginTop: '5px'}}>
         <Grid item sx={{marginRight: '15px'}}>
           <Typography variant='body1'>
               Highpass
